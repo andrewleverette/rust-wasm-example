59a78751cc0387290fe20b2cd8e907c4f3f9b7c2
andrewleverette <andrew.leverette@decisionsource.com>
2020-05-06 10:02:24 -0500

    Add test to generate universe with space ship

--- src/lib.rs
+++ src/lib.rs
@@ -53,6 +53,20 @@ impl Universe {
         self.cells.as_slice().as_ptr()
     }
 
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        let size = (width * self.height) as usize;
+        self.cells = FixedBitSet::with_capacity(size);
+        self.cells.set_range(0..size, false);
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        let size = (self.width * height) as usize;
+        self.cells = FixedBitSet::with_capacity(size);
+        self.cells.set_range(0..size, false);
+    }
+
     pub fn tick(&mut self) {
         let mut next = self.cells.clone();
 
@@ -103,5 +117,18 @@ impl Universe {
             }
         }
         count
+    }
+}
+
+impl Universe {
+    pub fn get_cells(&self) -> &FixedBitSet {
+        &self.cells
+    }
+
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells.set(idx, true);
+        }
     }
 }
